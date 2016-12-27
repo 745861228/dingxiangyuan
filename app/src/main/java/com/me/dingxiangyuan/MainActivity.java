fe58eec41b7df9fb0277f49c03ae99f287f28114
往事如烟 <745861228@qq.com>
2016-12-27 23:57:09 +0800

    缓存框架

--- app/src/main/java/com/me/dingxiangyuan/MainActivity.java
+++ app/src/main/java/com/me/dingxiangyuan/MainActivity.java
@@ -2,6 +2,9 @@ package com.me.dingxiangyuan;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.me.dingxiangyuan.base.BaseData;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BaseData baseData = new BaseData() {
+            @Override
+            public void setResultData(String data) {
+                Toast.makeText(MainActivity.this, data, Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        baseData.getData("http://www.sougou.com",null,0,BaseData.NORMALTIME);
+
     }
 }
