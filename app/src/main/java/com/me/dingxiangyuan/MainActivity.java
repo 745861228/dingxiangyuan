d29ed3958fc559227d9a01cccdcacc3839cc4280
往事如烟 <745861228@qq.com>
2016-12-28 10:04:10 +0800

    框架修改

--- app/src/main/java/com/me/dingxiangyuan/MainActivity.java
+++ app/src/main/java/com/me/dingxiangyuan/MainActivity.java
@@ -4,23 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
-import com.me.dingxiangyuan.base.BaseData;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        BaseData baseData = new BaseData() {
-            @Override
-            public void setResultData(String data) {
-                Toast.makeText(MainActivity.this, data, Toast.LENGTH_SHORT).show();
-            }
-        };
-
-        baseData.getData("http://www.sougou.com",null,0,BaseData.NORMALTIME);
-
     }
 }
