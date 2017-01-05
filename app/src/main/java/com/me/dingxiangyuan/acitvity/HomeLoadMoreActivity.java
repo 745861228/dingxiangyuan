23050d91329e49fc330b94c54ebfc8b11d1e0eac
往事如烟 <745861228@qq.com>
2017-01-05 21:48:38 +0800

    主界面更多  上拉加载下拉刷新  2017.01.05

---
+++ app/src/main/java/com/me/dingxiangyuan/acitvity/HomeLoadMoreActivity.java
@@ -0,0 +1,124 @@
+package com.me.dingxiangyuan.acitvity;
+
+import android.graphics.Color;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import com.google.gson.Gson;
+import com.me.dingxiangyuan.R;
+import com.me.dingxiangyuan.adapter.LoadMoreRvAdapater;
+import com.me.dingxiangyuan.base.BaseActivity;
+import com.me.dingxiangyuan.base.BaseData;
+import com.me.dingxiangyuan.bean.LoadMoreBean;
+import com.me.dingxiangyuan.utils.CommonUtils;
+import com.me.dingxiangyuan.utils.UrlUtils;
+import com.me.dingxiangyuan.view.HomeLoadMoreListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeLoadMoreActivity extends BaseActivity implements SwipeRefreshLayout.OnRefreshListener {
+
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private RecyclerView lordMoreRecyclerView;
+    private boolean isFlag = false;
+    private List<LoadMoreBean.DataBean> dataBeanArrayList = new ArrayList<>();
+    private int page = -1;
+    private LoadMoreRvAdapater loadMoreRvAdapater;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_load_more);
+        //初始化数据
+        initView();
+    }
+
+    private void initView() {
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout);
+        lordMoreRecyclerView = (RecyclerView) findViewById(R.id.lordMoreRecyclerView);
+
+        //设置swipeRefreshLayout进度条颜色
+        swipeRefreshLayout.setColorSchemeColors(Color.BLUE);
+        //设置布局管理
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        lordMoreRecyclerView.setLayoutManager(linearLayoutManager);
+        //设置进来就刷新数据
+        swipeRefreshLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                isFlag = true;
+                page = 1;
+                swipeRefreshLayout.setRefreshing(true);
+                initDatas();
+            }
+        });
+        //设置课下拉刷新
+        swipeRefreshLayout.setOnRefreshListener(this);
+        //对recycleView添加一个滑动的监听
+         lordMoreRecyclerView.addOnScrollListener(new HomeLoadMoreListener(linearLayoutManager) {
+             @Override
+             public void onLoadMore() {
+                 isFlag = false;
+                 initDatas();
+                 page++;
+             }
+         });
+    }
+
+    private void initDatas() {
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(2000);
+                    new BaseData() {
+                        @Override
+                        public void setResultData(final String response) {
+                            if (response != null) {
+                                CommonUtils.runOnMainThread(new Runnable() {
+
+                                    @Override
+                                    public void run() {
+                                        //解析数据
+                                        Gson gson = new Gson();
+                                        LoadMoreBean loadMoreBean = gson.fromJson(response, LoadMoreBean.class);
+                                        if (isFlag) {
+                                            swipeRefreshLayout.setRefreshing(false);
+                                            dataBeanArrayList.clear();
+                                            dataBeanArrayList.addAll(loadMoreBean.data);
+                                        } else {
+                                            dataBeanArrayList.addAll(loadMoreBean.data);
+                                        }
+                                        if (loadMoreRvAdapater == null) {
+                                            loadMoreRvAdapater = new LoadMoreRvAdapater(dataBeanArrayList, HomeLoadMoreActivity.this);
+                                            lordMoreRecyclerView.setAdapter(loadMoreRvAdapater);
+                                        } else {
+                                            loadMoreRvAdapater.notifyDataSetChanged();
+                                        }
+
+                                    }
+                                });
+                            }
+                        }
+                    }.getData(UrlUtils.LOADMORE + page, BaseData.NOTIME, null, 0);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+
+
+    /**
+     * 下拉数显操作
+     */
+    @Override
+    public void onRefresh() {
+        page = 1;
+        initDatas();
+    }
+}
---
+++ app/src/main/java/com/me/dingxiangyuan/adapter/LoadMoreRvAdapater.java
@@ -0,0 +1,86 @@
+package com.me.dingxiangyuan.adapter;
+
+import android.animation.ObjectAnimator;
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.me.dingxiangyuan.R;
+import com.me.dingxiangyuan.bean.LoadMoreBean;
+import com.me.dingxiangyuan.holder.BaseHolder;
+import com.me.dingxiangyuan.holder.LoadMoreHolder;
+import com.me.dingxiangyuan.holder.LoadMoreRvHolder;
+import com.me.dingxiangyuan.utils.CommonUtils;
+import com.zhy.autolayout.utils.L;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.me.dingxiangyuan.R.id.view;
+
+/**
+ * author by LiKe on 2017/1/5.
+ */
+
+public class LoadMoreRvAdapater extends RecyclerView.Adapter<BaseHolder> {
+
+    private Context context;
+    private List<LoadMoreBean.DataBean> arrayList;
+    private final int NORMAL = 0;
+    private final int LOADMORE = 1;
+    private int lastPosition = -1;
+
+
+    public LoadMoreRvAdapater(List<LoadMoreBean.DataBean> arrayList, Context context) {
+        this.arrayList = arrayList;
+        this.context = context;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == arrayList.size()) {
+            return LOADMORE;
+        }
+        return NORMAL;
+    }
+
+    @Override
+    public BaseHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = null;
+        BaseHolder holder = null;
+        switch (viewType) {
+            case NORMAL:
+                view = CommonUtils.inflate(R.layout.loadmore_rv_item);
+
+                holder = new LoadMoreRvHolder(view);
+                break;
+            case LOADMORE:
+                view = CommonUtils.inflate(R.layout.progress_item);
+                holder = new LoadMoreHolder(view);
+                break;
+        }
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(BaseHolder holder, int position) {
+        if (position < arrayList.size()) {
+            LoadMoreRvHolder loadMoreHolder = (LoadMoreRvHolder) holder;
+
+            loadMoreHolder.setData(context, arrayList.get(position));
+            if (position>lastPosition){
+                ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(loadMoreHolder.itemView, View.TRANSLATION_Y, 600, 200, 50, 0);
+                objectAnimator.setDuration(500);
+                objectAnimator.start();
+                lastPosition = position;
+            }
+
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return arrayList.size() + 1;
+    }
+}
