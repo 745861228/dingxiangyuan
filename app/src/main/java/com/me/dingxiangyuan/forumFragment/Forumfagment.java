6363cb0f1d5d556e0b7153703b8fc29a2f95dcee
guotao123456 <1064160005@qq.com>
2017-01-09 08:34:07 +0800

    郭涛提交  精选和全部详情 界面                               2017.01.09

--- app/src/main/java/com/me/dingxiangyuan/forumFragment/Forumfagment.java
+++ app/src/main/java/com/me/dingxiangyuan/forumFragment/Forumfagment.java
@@ -1,20 +1,22 @@
 package com.me.dingxiangyuan.forumFragment;
 
+import android.content.Intent;
 import android.view.View;
-
 import com.me.dingxiangyuan.R;
+import com.me.dingxiangyuan.acitvity.BanKuaiActivity;
 import com.me.dingxiangyuan.base.BaseData;
 import com.me.dingxiangyuan.base.BaseFragment;
 import com.me.dingxiangyuan.utils.CommonUtils;
 import com.me.dingxiangyuan.view.ShowingPage;
-
+import com.zhy.autolayout.AutoLinearLayout;
 /**
  * Created by Administrator on 2016/12/30.
  */
-
-public class Forumfagment extends BaseFragment{
+public class Forumfagment extends BaseFragment implements View.OnClickListener {
     public  String  data;
     private MyHomeData myHomeData;
+    private AutoLinearLayout bankuai_layout;
+
     @Override
     protected void onLoad() {
         myHomeData = new MyHomeData();
@@ -24,7 +26,6 @@ public class Forumfagment extends BaseFragment{
     @Override
     public View createSuccessView() {
         View view=initView();
-
         return view;
     }
     /**
@@ -33,11 +34,20 @@ public class Forumfagment extends BaseFragment{
      */
     private View initView() {
         View inflate = CommonUtils.inflate(R.layout.forum_layout_item);
-
+        bankuai_layout = (AutoLinearLayout) inflate.findViewById(R.id.bankuai_layout);
+        bankuai_layout.setOnClickListener(this);
         return inflate;
     }
-
-
+    @Override
+    public void onClick(View v) {
+                   switch (v.getId())
+                   {
+                       case R.id.bankuai_layout:
+                           Intent intent=new Intent(getActivity(), BanKuaiActivity.class);
+                           startActivity(intent);
+                           break;
+                   }
+    }
     /**
      * 请求网络
      */
